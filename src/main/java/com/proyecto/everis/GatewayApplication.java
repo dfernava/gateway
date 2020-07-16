09763922ddfd9dfd248332c4796da968ce5f513e
dfernava <67983816+dfernava@users.noreply.github.com>
2020-07-16 18:26:56 -0500

    16-07

--- src/main/java/com/proyecto/everis/GatewayApplication.java
+++ src/main/java/com/proyecto/everis/GatewayApplication.java
@@ -3,11 +3,11 @@ package com.proyecto.everis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
+@EnableHystrix
 public class GatewayApplication {
 
 	public static void main(String[] args) {
